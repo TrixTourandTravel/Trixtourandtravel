258c05611535bc64649bbeca09041fc4fb6bb04f
Trix Tour and Travel <trixtourandtravel@gmail.com>
2026-04-22 19:59:07 +0200

    Update slideshow.js

--- slideshow.js
+++ slideshow.js
@@ -1,22 +1,15 @@
-let slides = document.querySelectorAll(".slideshow img");
-let currentIndex = 0;
-
-function showSlide(index) {
-  slides.forEach((slide, i) => {
-    slide.classList.remove("active");
-    if (i === index) {
-      slide.classList.add("active");
-    }
-  });
-}
-
-function nextSlide() {
-  currentIndex = (currentIndex + 1) % slides.length;
-  showSlide(currentIndex);
-}
-
-// Show the first slide initially
-showSlide(currentIndex);
-
-// Change slide every 3 seconds
-setInterval(nextSlide, 3000);
+<div class="slideshow">
+  <div class="slide">
+    <img src="images/visa.jpg" alt="Visa Consultation">
+    <div class="caption">Visa Consultation</div>
+  </div>
+  <div class="slide">
+    <img src="images/travel.jpg" alt="Travel Planning">
+    <div class="caption">Travel Planning</div>
+  </div>
+  <div class="slide">
+    <img src="images/canada.jpg" alt="Study in Canada">
+    <div class="caption">Study in Canada</div>
+  </div>
+</div>
+<script src="slideshow.js"></script>
